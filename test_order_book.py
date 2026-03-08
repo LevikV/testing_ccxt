08c33d2a708c04f67c48246027256f0cc1cbdc21
LevikV <alex@kplus79.ru>
2026-03-08 13:07:47 +1000

    working

---
+++ test_order_book.py
@@ -0,0 +1,12 @@
+import ccxt_patch
+import asyncio
+
+async def main():
+    exchange_client = ccxt_patch.weex()
+    data = await exchange_client.fetch_order_book('BTC/USDT:USDT')
+
+    print(data)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
